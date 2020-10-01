765bfa8905a32c1e300143e00629c3710f16d323
Lillie <lillieuph@gmail.com>
2020-10-01 16:18:52 -0400

    Add new properties to CurrentPlayer, create grid for Player Data in the MainWindow and display the values from properties of the CurrentPlayer object

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 using Engine.Models;
 
-namespace Engine.Models.ViewModels
+namespace Engine.ViewModels
 {
-    class GameSession
+    public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
 
         public GameSession()
         {
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Lillie";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
             CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
         }
     }
 }
