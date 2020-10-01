ebab6b4263105a395a578d1464f80ea9e06af196
Lillie <lillieuph@gmail.com>
2020-10-01 15:21:47 -0400

    Initial commit

---
+++ Engine/Models/ViewModels/GameSession.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine.Models;
+
+namespace Engine.Models.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Lillie";
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
